cb8911d69a55204a2c5c3bcd1c34f64c418d6f40
Maximilian Köhl <koehl@silitics.com>
2026-03-19 12:56:05 +0100

    refine systemd services

--- recipes/setup-apps-services/steps/00-install.sh
+++ recipes/setup-apps-services/steps/00-install.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
-install -D -m 644 "${RECIPE_DIR}/files/rugix-app-sync.service" -t /usr/lib/systemd/system/
-install -D -m 644 "${RECIPE_DIR}/files/rugix-app-recover.service" -t /usr/lib/systemd/system/
-install -D -m 644 "${RECIPE_DIR}/files/rugix-app-gc.service" -t /usr/lib/systemd/system/
-install -D -m 644 "${RECIPE_DIR}/files/rugix-app-gc.timer" -t /usr/lib/systemd/system/
+install -D -m 644 "${RECIPE_DIR}/files/rugix-apps-restore-units.service" -t /usr/lib/systemd/system/
+install -D -m 644 "${RECIPE_DIR}/files/rugix-apps-recover.service" -t /usr/lib/systemd/system/
+install -D -m 644 "${RECIPE_DIR}/files/rugix-apps-gc.service" -t /usr/lib/systemd/system/
+install -D -m 644 "${RECIPE_DIR}/files/rugix-apps-gc.timer" -t /usr/lib/systemd/system/
 
-systemctl enable rugix-app-sync
-systemctl enable rugix-app-recover
-systemctl enable rugix-app-gc.timer
+systemctl enable rugix-apps-restore-units
+systemctl enable rugix-apps-recover
+systemctl enable rugix-apps-gc.timer
